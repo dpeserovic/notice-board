d32337c9a726d89d949dd7e70d5e32b63d340861
dpeserovic <d.peserovic@gmail.com>
2023-01-10 16:46:19 +0100

    implement main layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,5 +1,14 @@
+import React from 'react';
+import { MainLayout } from 'common/components';
+
 function App() {
-  return <>App</>
+  return (
+    <MainLayout>
+      <>
+        Notice Board
+      </>
+    </MainLayout>
+  );
 }
 
 export default App;
