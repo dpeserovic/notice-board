ae4f8a9966077daa7fb712e539be551f738b15ad
dpeserovic <d.peserovic@gmail.com>
2023-01-11 14:12:57 +0100

    implement router

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { RouterContext, RouterView } from 'mobx-state-router';
 import { MainLayout } from 'common/components';
+import { LoginPage, RegisterPage } from './modules/membership/pages';
 
-function App() {
+const routeNames = {
+  login: <LoginPage />,
+  register: <RegisterPage />,
+}
+
+function App({ rootStore }) {
+  const { routerStore: { router } } = rootStore;
   return (
-    <MainLayout>
-      <>
-        Notice Board
-      </>
-    </MainLayout>
+    <RouterContext.Provider value={router}>
+      <MainLayout>
+        <RouterView viewMap={routeNames} />
+      </MainLayout>
+    </RouterContext.Provider>
   );
 }
 
