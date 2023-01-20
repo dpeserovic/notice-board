69640c2c2563955e87f7c9f93eca14e12e38cb95
dpeserovic <d.peserovic@gmail.com>
2023-01-20 19:13:30 +0100

    implement auth and membership service

--- web/src/common/baas/Baas.js
+++ web/src/common/baas/Baas.js
@@ -1,9 +1,18 @@
 import { initializeApp } from 'firebase/app';
+import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth';
+
 
 class Baas {
     constructor(baasConfig) {
         this.app = initializeApp(baasConfig);
+        this.auth = getAuth(this.app);
+        this.membershipService = this.createMembershipService();
     }
+
+    createMembershipService = () => ({
+        login: signInWithEmailAndPassword,
+        register: createUserWithEmailAndPassword,
+    })
 }
 
 export default Baas;
