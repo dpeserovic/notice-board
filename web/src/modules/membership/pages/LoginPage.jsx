170c2625676169344840334977bebe049acaa4fb
dpeserovic <d.peserovic@gmail.com>
2023-01-20 21:00:59 +0100

    implement login

--- web/src/modules/membership/pages/LoginPage.jsx
+++ web/src/modules/membership/pages/LoginPage.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import { RBInput } from 'common/components';
+import { defaultTemplate } from 'common/hoc';
 
-function LoginPage() {
+function LoginPage(props) {
+    const { membershipModuleStore: { loginViewStore: { loginForm, goToRegister } } } = props;
     return (
-        <>
-            LoginPage
-        </>
+        <div className="center">
+            <h1>Login</h1>
+            <Form>
+                {Array.from(loginForm.fields).map(([key, value]) => {
+                    return (
+                        <Row key={key} className="justify-content-md-center mb-3" md={5}>
+                            <Form.Group>
+                                <RBInput {...loginForm.$(key).bind()} />
+                            </Form.Group>
+                        </Row>
+                    )
+                })}
+                <Button type="button" className="mb-3" variant="danger" onClick={loginForm.onSubmit}>Log in</Button>
+            </Form>
+            <Button type="button" variant="warning" onClick={goToRegister}>Don't have an account? Register</Button>
+        </div>
     );
 }
 
-export default LoginPage;
+export default defaultTemplate(LoginPage);
--- web/src/modules/membership/stores/LoginViewStore.js
+++ web/src/modules/membership/stores/LoginViewStore.js
@@ -1,5 +1,44 @@
+import { createForm } from 'common/form';
+import { loginFormFields } from '../forms';
+
 class LoginViewStore {
+    loginForm = null;
+
     constructor(rootStore) {
+        this.globalLoaderStore = rootStore.globalLoaderStore;
+        this.auth = rootStore.authStore.auth;
+        this.membershipService = rootStore.authStore.membershipService;
+        this.notificationStore = rootStore.notificationStore;
+        this.routerStore = rootStore.routerStore;
+    }
+
+    init = () => {
+        this.loginForm = createForm({ fields: loginFormFields, hooks: { onSuccess: this.handleLoginFormSuccess, onError: this.handleLoginFormError } })
+    }
+
+    handleLoginFormSuccess = async form => {
+        try {
+            this.globalLoaderStore.suspend();
+            const { email, password } = form.values();
+            await this.membershipService.login(this.auth, email, password);
+            this.notificationStore.showSuccessToast('Success');
+        } catch (e) {
+            this.notificationStore.showErrorToast('Error');
+        } finally {
+            this.globalLoaderStore.resume();
+        }
+    }
+
+    handleLoginFormError = () => {
+        this.notificationStore.showErrorToast('Invalid form values');
+    }
+
+    goToRegister = () => {
+        this.routerStore.goTo('register');
+    }
+
+    dispose = () => {
+        this.loginForm = null;
     }
 }
 
